2ba4f60937c185202603f97a44fed3eb645b353a
jonny <jonny.perez.u@gmail.com>
2018-04-30 13:35:16 -0500

    formulario

---
+++ resources/views/form.blade.php
@@ -0,0 +1,137 @@
+@extends('layout')
+
+@section('type', 'profile-page')
+
+@section('header')
+    <h1 class="title">PROFACAD CUValles</h1>
+    <h4>Programa de Formación, Actualización y Capacitación Docente</h4>
+@stop
+
+@section('content')
+
+    <h1>Tabla</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th class="text-center">#</th>
+            <th>Name</th>
+            <th>Job Position</th>
+            <th>Since</th>
+            <th class="text-right">Salary</th>
+            <th class="text-right">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+            <td class="text-center">1</td>
+            <td>Andrew Mike</td>
+            <td>Develop</td>
+            <td>2013</td>
+            <td class="text-right">&euro; 99,225</td>
+            <td class="td-actions text-right">
+                <button type="button" rel="tooltip" class="btn btn-info">
+                    <i class="material-icons">person</i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-success">
+                    <i class="material-icons">edit</i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger">
+                    <i class="material-icons">close</i>
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <td class="text-center">2</td>
+            <td>John Doe</td>
+            <td>Design</td>
+            <td>2012</td>
+            <td class="text-right">&euro; 89,241</td>
+            <td class="td-actions text-right">
+                <button type="button" rel="tooltip" class="btn btn-info btn-round">
+                    <i class="material-icons">person</i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-success btn-round">
+                    <i class="material-icons">edit</i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-round">
+                    <i class="material-icons">close</i>
+                </button>
+            </td>
+        </tr>
+        <tr>
+            <td class="text-center">3</td>
+            <td>Alex Mike</td>
+            <td>Design</td>
+            <td>2010</td>
+            <td class="text-right">&euro; 92,144</td>
+            <td class="td-actions text-right">
+                <button type="button" rel="tooltip" class="btn btn-info btn-simple">
+                    <i class="material-icons">person</i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-success btn-simple">
+                    <i class="material-icons">edit</i>
+                </button>
+                <button type="button" rel="tooltip" class="btn btn-danger btn-simple">
+                    <i class="material-icons">close</i>
+                </button>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+    <h1>Formulario</h1>
+    <form>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputEmail4">Email</label>
+                <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+            </div>
+            <div class="form-group col-md-6">
+                <label for="inputPassword4">Password</label>
+                <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="inputAddress">Address</label>
+            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+        </div>
+        <div class="form-group">
+            <label for="inputAddress2">Address 2</label>
+            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+        </div>
+        <div class="form-row">
+            <div class="form-group col-md-6">
+                <label for="inputCity">City</label>
+                <input type="text" class="form-control" id="inputCity">
+            </div>
+            <div class="form-group col-md-4">
+                <label for="inputState">State</label>
+                <select id="inputState" class="form-control">
+                    <option selected>Choose...</option>
+                    <option>...</option>
+                </select>
+            </div>
+            <div class="form-group col-md-2">
+                <label for="inputZip">Zip</label>
+                <input type="text" class="form-control" id="inputZip">
+            </div>
+        </div>
+        <div class="form-group">
+            <div class="form-check">
+                <label class="form-check-label">
+                    <input class="form-check-input" type="checkbox" value="">
+                    Check me out
+                    <span class="form-check-sign">
+            <span class="check"></span>
+          </span>
+                </label>
+            </div>
+        </div>
+        <!-- input with datetimepicker -->
+        <div class="form-group">
+            <label class="label-control">Datetime Picker</label>
+            <input type="text" class="form-control datetimepicker" />
+        </div>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+    </form>
+
+@stop
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('index');
 });
+Route::get('/form',function (){
+   return view('form');
+});
